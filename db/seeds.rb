ecf3eb9e071edacafc976f69f93d1767c6973b8e
Nicolas Guimier <nicolas.g@flexishore.com>
2019-11-22 15:33:48 +0100

    Add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,15 +25,36 @@ puts 'End ingredients'
 
 puts '->  Start cocktails'
 
-cocktails = [
-  { name: 'Mojito' },
-  { name: 'Long Island' },
-  { name: 'Mushi Tonic' },
-  { name: 'Bière Mayo' }
-]
-Cocktail.create!(cocktails)
-puts "#{Cocktail.count} cocktails imported."
+url_mojito      = 'https://res.cloudinary.com/flexishore/image/upload/v1574431219/mister-coktail/Mojito_nc1p6f.jpg'
+url_long_island = 'https://res.cloudinary.com/flexishore/image/upload/v1574431224/mister-coktail/long-island_ubhgmb.jpg'
+url_mushi_tonic = 'https://res.cloudinary.com/flexishore/image/upload/v1574431218/mister-coktail/mushi-tonic_k4whz1.jpg'
+url_bier_mayo   = 'https://res.cloudinary.com/flexishore/image/upload/v1574431217/mister-coktail/biere-frites_mxkdac.jpg'
 
+mojito = Cocktail.new(
+  name: 'Mojito',
+  description: 'Libero vero beatae voluptate asperiores, placeat nemo dignissimos, repellat suscipit deleniti minima nam nihil pariatur optio facilis doloremque doloribus. Porro, similique, aut.')
+mojito.remote_photo_url = url_mojito
+mojito.save!
+
+long_island = Cocktail.new(
+  name: 'Long Island',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt rem maiores at.')
+long_island.remote_photo_url = url_long_island
+long_island.save!
+
+mushi_tonic = Cocktail.new(
+  name: 'Mushi Tonic',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quae doloribus obcaecati recusandae aliquid facere, non sed quaerat neque laudantium voluptas ab animi suscipit quia error eius iusto! Perspiciatis, assumenda?')
+mushi_tonic.remote_photo_url = url_mushi_tonic
+mushi_tonic.save!
+
+bier_mayo = Cocktail.new(
+  name: 'Bière Mayo',
+  description: 'Le booster de votre petit déj')
+bier_mayo.remote_photo_url = url_bier_mayo
+bier_mayo.save!
+
+puts "#{Cocktail.count} cocktails imported."
 
 puts 'End Cocktails'
 
